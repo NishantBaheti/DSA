54224a483869a6354afc5e0fd2b082375af60db2
Nishant Baheti <nishantbaheti.it19@gmail.com>
2021-03-05 03:39:08 +0530

    progress commit

---
+++ linked_list.py
@@ -0,0 +1,72 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self,value):
+        """Insert at the end
+
+        Args:
+            value (object): Value to insert in linked list
+        """
+        temp = Node(value)
+        if self.head is None:
+            self.head = temp
+        elif self.tail is None:
+            self.tail = temp
+            self.head.next = self.tail
+        else:
+            self.tail.next = temp
+            self.tail = self.tail.next
+
+
+    def push(self,value):
+        """Push ar the start after head
+
+        Args:
+            value (object): Value to push to list
+        """
+        temp = Node(value)
+        if self.head is None:
+            self.head = temp
+        elif self.tail is None:
+            self.tail = temp
+            self.head.next = self.tail
+        else:
+            temp.next = self.head.next
+            self.head.next = temp
+
+    def traverse(self):
+        """Traverse the linked list
+
+        Returns:
+            node_values (list) : linked list values
+        """
+        node_values = []
+        current_node = self.head
+        while current_node:
+            node_values.append(current_node.data)
+            current_node = current_node.next
+
+        return node_values
+
+
+if __name__ == "__main__":
+    
+    l_list = LinkedList()
+
+    for i in range(10):
+        l_list.push("push"+str(i))
+
+    for i in range(10):
+        l_list.insert("insert"+str(i))
+
+
+    print(l_list.traverse())
